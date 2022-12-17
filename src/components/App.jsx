3618a18c18fb634f913e9c5ce6a1a34ae7be04df
Vladyslav Solomakha <vladislavsolomaha08@gmail.com>
2022-12-17 19:13:37 +0200

    solved 2, 3, 4 tasks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,12 @@
 import { Profile } from 'components/Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
-import user from '../user.json';
+import user from '../jsonFiles/user.json';
+import data from '../jsonFiles/data.json';
+import friends from '../jsonFiles/friends.json';
+import transactions from '../jsonFiles/transactions.json';
 
 export const App = () => {
   const { username, tag, location, stats, avatar } = user;
@@ -14,6 +20,9 @@ export const App = () => {
         avatar={avatar}
         stats={stats}
       />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />;
+      <TransactionHistory items={transactions} />;
     </>
   );
 };
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,41 @@
+import PropTypes from 'prop-types';
+import css from './TransactionHistory.module.css';
+
+export const TransactionHistory = ({ items }) => {
+  return (
+    <table className={css.transactionHistory}>
+      <thead className={css.thead}>
+        <tr>
+          <th className={css.th}>Type</th>
+          <th className={css.th}>Amount</th>
+          <th className={css.th}>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(({ id, type, amount, currency }, idx) => {
+          const even = idx % 2 !== 0;
+
+          return (
+            <tr key={id} className={(even && css.even).toString()}>
+              <td className={css.td}>{type}</td>
+              <td className={css.td}>{amount}</td>
+              <td className={css.td}>{currency}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
